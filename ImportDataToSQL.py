b61eea6a9e10c8e302b092b11ab1714f76c46e57
ywyz <admin@ywyz.tech>
2022-01-27 11:48:34 +0800

    插入旧表成功

--- ImportDataToSQL.py
+++ ImportDataToSQL.py
@@ -1,8 +1,38 @@
 '''
 Author: ywyz
 Date: 2022-01-25 09:52:01
-LastEditTime: 2022-01-25 09:52:02
+LastEditTime: 2022-01-27 11:46:48
 LastEditors: ywyz
 Description: 将数据导出至mysql数据库
 FilePath: /StudentsInfo/ImportDataToSQL.py
 '''
+
+from openpyxl import load_workbook
+from Students import Students
+
+
+def InsertOldInfo():
+    # 插入旧表
+    filepath = '2020.9.1防疫信息表.xlsx'
+    wb = load_workbook(filename=filepath)
+    sheetName = "学生"
+    ws = wb[sheetName]
+
+    for row in ws.rows:
+        if row[0].value == "序号":
+            continue
+
+        else:
+
+            student = Students(row[3].value, '男', row[7].value, row[8].value,
+                               row[4].value, row[5].value, row[11].value, ' ',
+                               '0')
+            student.InsertIntoSql()
+    print("插入成功")
+
+
+InsertOldInfo()
+
+
+def insertNewInfo():
+    """插入新表"""
--- Students.py
+++ Students.py
@@ -1,44 +1,65 @@
 '''
 Author: your name
 Date: 2022-01-25 11:08:08
-LastEditTime: 2022-01-25 15:15:27
-LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+LastEditTime: 2022-01-27 11:10:55
+LastEditors: ywyz
+Description:学生类型，包括名字，年级，班级，父母姓名，家庭住址，手机号码
 FilePath: /StudentsInfo/Students.py
 '''
-"""
-学生类型，包括名字，年级，班级，父母姓名，家庭住址，手机号码
-"""
+
+import pymysql
+# 打开数据库链接
+db = pymysql.connect(host="localhost",
+                     user="StudentInfo",
+                     password="yw980924@163.com",
+                     database="StudentInfo")
+cursor = db.cursor()
+
 
 class Students():
-    def __init__(self, name, grade, classes, parent_name, phone, address, isOut):
+    def __init__(self, name, sex, grade, classes, parent_name, phone, address,
+                 employer, isOut):
         self.name = name
-        self.name = grade
+        self.grade = grade
         self.classes = classes
         self.parent_name = parent_name
         self.phone = phone
         self.address = address
         self.isOut = isOut
+        self.sex = sex
+        self.employer = employer
 
     def get_name(self):
         return self.name
 
+    def get_sex(self):
+        return self.sex
+
     def get_grade(self):
         return self.grade
-    
+
     def get_classes(self):
         return self.classes
 
     def get_parent_name(self):
-        return parent_name
-    
+        return self.parent_name
+
     def get_phone(self):
-        return phone
-    
+        return self.phone
+
     def get_address(self):
-        return address
+        return self.address
 
     def get_isOut(self):
-        return isOut
+        return self.isOut
 
-    def Insert
+    def get_employer(self):
+        return self.employer
+
+    def InsertIntoSql(self):
+        sqlInsert = "Insert into basicinfo (name, sex, grade, classes, parentname, phone, address, employer, isOut) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (
+            self.name, self.sex, self.grade, self.classes, self.parent_name,
+            self.phone, self.address, self.employer, self.isOut)
+
+        cursor.execute(sqlInsert)
+        db.commit()
